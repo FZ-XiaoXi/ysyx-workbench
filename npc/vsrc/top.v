6ce818e5a9e7f4484b1f6275ce1553ca071d0a12
tracer-ysyx <tracer@ysyx.org>
2025-12-14 21:33:54 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:33:54 up  2:06,  1 user,  load average: 0.39, 0.59, 0.53

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -7,7 +7,7 @@ module top(
   always @(posedge clk) begin
     if (rst) begin led <= 1; count <= 0; end
     else begin
-      if (count == 0) led <= {led[14:0], led[15]};
+      if (count == 0) led <= {led[0], led[15:1]};
       count <= (count >= 5000000 ? 32'b0 : count + 1);
     end
   end
