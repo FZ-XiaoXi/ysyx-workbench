681949fda95f5c4c6b098b5f0f3e103bf1258e2f
tracer-ysyx <tracer@ysyx.org>
2025-12-14 20:57:03 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:57:03 up  1:29,  1 user,  load average: 0.63, 0.51, 0.32

---
+++ npc/csrc/sim_main.cpp
@@ -0,0 +1,36 @@
+#include "Vtop.h"
+#include <stdlib.h>
+#include "verilated_vcd_c.h"
+#include <stdio.h>
+#include <assert.h>
+#include "verilated.h"
+#include <nvboard.h>
+void nvboard_bind_all_pins(Vtop* top);
+int main(int argc, char** argv) {
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+	Verilated::traceEverOn(true);
+    Vtop* top = new Vtop{contextp};
+	VerilatedVcdC* tfp = new VerilatedVcdC;
+	top->trace(tfp,99);
+	tfp->open("wave.vcd");
+	nvboard_bind_all_pins(top);
+	nvboard_init();
+	while (!contextp->gotFinish()) {
+		int a=rand()&1;
+		int b=rand()&1;
+		top->a=a;
+		top->b=b;
+		top->eval();
+		nvboard_update();
+		tfp->dump(contextp->time());
+		contextp->timeInc(1);
+		printf("a=%d b=%d f=%d\n",a,b,top->f);
+		assert(top->f == (a^b));
+	}
+	tfp->close();
+    delete top;
+    delete contextp;
+	nvboard_quit();
+    return 0;
+}
