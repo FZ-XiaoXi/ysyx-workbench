5afe532f334fc276371a07a026e98dfde4784f66
tracer-ysyx <tracer@ysyx.org>
2026-01-01 22:30:35 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:30:35 up  1:39,  1 user,  load average: 2.04, 1.72, 1.55

---
+++ npc/obj_dir/Vencode42__ConstPool_0.cpp
@@ -0,0 +1,30 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Constant pool
+//
+
+#include "verilated.h"
+
+extern const VlUnpacked<IData/*31:0*/, 32> Vencode42__ConstPool__TABLE_h220cfc1d_0 = {{
+    0U, 4U, 0U, 4U,
+    0U, 4U, 0U, 4U,
+    0U, 4U, 0U, 4U,
+    0U, 4U, 0U, 4U,
+    0U, 4U, 0U, 4U,
+    0U, 4U, 0U, 4U,
+    0U, 4U, 0U, 4U,
+    0U, 4U, 0U, 4U
+}};
+
+extern const VlUnpacked<CData/*1:0*/, 32> Vencode42__ConstPool__TABLE_h71f80a71_0 = {{
+    1U, 3U, 1U, 3U, 1U, 3U, 1U, 3U,
+    1U, 3U, 1U, 3U, 1U, 3U, 1U, 3U,
+    1U, 3U, 1U, 3U, 1U, 3U, 1U, 3U,
+    1U, 3U, 1U, 3U, 1U, 3U, 1U, 3U
+}};
+
+extern const VlUnpacked<CData/*1:0*/, 32> Vencode42__ConstPool__TABLE_hfc4e910a_0 = {{
+    0U, 0U, 0U, 0U, 0U, 1U, 0U, 1U,
+    0U, 2U, 0U, 2U, 0U, 2U, 0U, 2U,
+    0U, 3U, 0U, 3U, 0U, 3U, 0U, 3U,
+    0U, 3U, 0U, 3U, 0U, 3U, 0U, 3U
+}};
