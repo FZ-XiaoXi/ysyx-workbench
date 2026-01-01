d73526d377d603e8660791d2caaeb90fcd3ddc0e
tracer-ysyx <tracer@ysyx.org>
2026-01-01 22:26:23 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:26:23 up  1:34,  1 user,  load average: 1.22, 1.39, 1.41

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -21,20 +21,20 @@ int main(int argc, char** argv) {
 	tfp->open("wave.vcd");
 	top->en=0b0;
 	top->x=0b1111;step_and_dump_wave();
-	for(int i=0;i<4;i++){
+	for(int j=0;j<4;j++){
 		top->x = top->x << 1;step_and_dump_wave();
 	}
 	top->x=0b1111;step_and_dump_wave();
-	for(int i=0;i<4;i++){
+	for(int j=0;j<4;j++){
 		top->x = (unsigned)(top->x) >> 1;step_and_dump_wave();
 	}
 	top->en=0b1;
 	top->x=0b1111;step_and_dump_wave();
-	for(int i=0;i<4;i++){
+	for(int j=0;j<4;j++){
 		top->x = top->x << 1;step_and_dump_wave();
 	}
 	top->x=0b1111;step_and_dump_wave();
-	for(int i=0;i<4;i++){
+	for(int j=0;j<4;j++){
 		top->x = (unsigned)(top->x) >> 1;step_and_dump_wave();
 	}
 	step_and_dump_wave();
