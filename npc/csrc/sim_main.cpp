a22b99293866c85383b35372728d4232942ace2e
tracer-ysyx <tracer@ysyx.org>
2026-01-01 22:03:11 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:03:11 up  1:11,  1 user,  load average: 1.42, 1.37, 1.30

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -12,8 +12,8 @@ void step_and_dump_wave(){
   tfp->dump(contextp->time());
 }
 void single_cycle(){
-	top->clk=0;top->eval();
-	top->clk=1;top->eval();
+	//top->clk=0;top->eval();
+	//top->clk=1;top->eval();
 }
 
 
