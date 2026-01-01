006fa83a24fab4666ea80b375086b7b7e510c210
tracer-ysyx <tracer@ysyx.org>
2026-01-01 23:27:32 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:27:32 up  2:35,  1 user,  load average: 0.22, 0.23, 0.35

--- npc/obj_dir/auto_bind.cpp
+++ npc/obj_dir/auto_bind.cpp
@@ -2,8 +2,9 @@
 #include "Vencode83.h"
 
 void nvboard_bind_all_pins(Vencode83* top) {
-	nvboard_bind_pin( &top->x, 4, SW3, SW2, SW1, SW0);
-	nvboard_bind_pin( &top->y, 2, LD1, LD0);
-	nvboard_bind_pin( &top->en, 1, SW4);
-	nvboard_bind_pin( &top->en, 1, SEG7A);
+	nvboard_bind_pin( &top->y, 3, LD2, LD1, LD0);
+	nvboard_bind_pin( &top->s, 1, LD4);
+	nvboard_bind_pin( &top->x, 8, SW7, SW6, SW5, SW4, SW3, SW2, SW1, SW0);
+	nvboard_bind_pin( &top->en, 1, SW8);
+	nvboard_bind_pin( &top->seg, 7, SEG7A, SEG7B, SEG7C, SEG7D, SEG7E, SEG7F, SEG7G);
 }
--- npc/vsrc/encode83.v
+++ npc/vsrc/encode83.v
@@ -1,14 +1,17 @@
-module encode83(x,en,y);
+module encode83(x,en,y,s,seg);
   input  [7:0] x;
   input  en;
-  output reg [1:0]y;
+  output reg [2:0]y;
+  output s;
+  output [6:0]seg;
   integer i;
   always @(x or en) begin
     if (en) begin
       y = 0;
-      for( i = 0; i <= 3; i = i+1)
-          if(x[i] == 1)  y = i[1:0];
+      for( i = 0; i <= 7; i = i+1)
+          if(x[i] == 1)  y = i[2:0];
     end
     else  y = 0;
   end
+  assign s=x||0;
 endmodule
