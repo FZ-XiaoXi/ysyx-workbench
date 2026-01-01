357d2e157ad1955e6920f1e9f179678bcab40705
tracer-ysyx <tracer@ysyx.org>
2026-01-01 22:54:29 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:54:29 up  2:02,  1 user,  load average: 1.54, 1.26, 1.26

--- npc/obj_dir/auto_bind.cpp
+++ npc/obj_dir/auto_bind.cpp
@@ -2,4 +2,7 @@
 #include "Vencode42.h"
 
 void nvboard_bind_all_pins(Vencode42* top) {
+	nvboard_bind_pin( &top->x, 4, SW0, SW1, SW2, SW3);
+	nvboard_bind_pin( &top->y, 2, LD0, LD1);
+	nvboard_bind_pin( &top->en, 1, SW4);
 }
