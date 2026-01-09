a4068c513159d1df0ae5d669d07556484d5c350e
tracer-ysyx <tracer@ysyx.org>
2026-01-09 17:12:25 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:12:25 up  5:57,  1 user,  load average: 0.81, 0.85, 0.87

--- npc-exercise/P6-1/vsrc/BarrelShifter.v
+++ npc-exercise/P6-1/vsrc/BarrelShifter.v
@@ -8,8 +8,8 @@ module BarrelShifter(
 	wire [7:0]shift1;
 	wire [7:0]shift2;
 	wire [7:0]shift4;
-	assign shift1=(shamt[0])?((!dir)?((mode)?({  din[7]    ,din[7:1]   }):({1'b0,din[7:1]   })):({din[6:0]   ,1'b0})):(din   );
-	assign shift2=(shamt[1])?((!dir)?((mode)?({2{shift1[7]},shift1[7:2]}):({2'b0,shift1[7:2]})):({shift1[5:0],2'b0})):(shift1);
-	assign shift4=(shamt[2])?((!dir)?((mode)?({4{shift2[7]},shift2[7:4]}):({4'b0,shift2[7:4]})):({shift2[3:0],4'b0})):(shift2);
+	assign shift1=(shamt[0])?((!dir)?((mode)?({   din[7]     ,din[7:1]   }):({1'b0,din[7:1]   })):({din[6:0]   ,1'b0})):(din   );
+	assign shift2=(shamt[1])?((!dir)?((mode)?({{2{shift1[7]}},shift1[7:2]}):({2'b0,shift1[7:2]})):({shift1[5:0],2'b0})):(shift1);
+	assign shift4=(shamt[2])?((!dir)?((mode)?({{4{shift2[7]}},shift2[7:4]}):({4'b0,shift2[7:4]})):({shift2[3:0],4'b0})):(shift2);
 	assign dout=shift4;
 endmodule
