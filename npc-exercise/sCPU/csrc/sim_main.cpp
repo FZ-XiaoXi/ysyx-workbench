3e67dc8a3c610034133f7673af2aa8e1001c84bc
tracer-ysyx <tracer@ysyx.org>
2026-01-09 21:47:45 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:47:45 up 10:32,  1 user,  load average: 1.39, 1.52, 1.22

--- npc-exercise/sCPU/csrc/sim_main.cpp
+++ npc-exercise/sCPU/csrc/sim_main.cpp
@@ -55,11 +55,13 @@ int main(int argc, char** argv) {
 		top->clk=0;
 		step_and_dump_wave();
 		tfp->dump(contextp->time());
-		contextp->timeInc(999999999);
+		contextp->timeInc(10);
+		for(int i=0;i<999999;i++);
 		top->clk=1;
 		step_and_dump_wave();
 		tfp->dump(contextp->time());
-		contextp->timeInc(999999999);
+		contextp->timeInc(10);
+		for(int i=0;i<999999;i++);
 		//printf("a=%d b=%d f=%d\n",a,b,top->f);
 		//assert(top->f == (a^b));
 	}
