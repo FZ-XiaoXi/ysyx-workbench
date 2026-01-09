8bb7f15c22e061d212cf4f12351dddb939ffdc3f
tracer-ysyx <tracer@ysyx.org>
2026-01-09 15:24:52 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:24:52 up  4:09,  1 user,  load average: 1.31, 1.24, 1.13

--- npc/vsrc/ALU.v
+++ npc/vsrc/ALU.v
@@ -21,7 +21,7 @@ module ALU(
 			3'b011:out=inA&inB;
 			3'b100:out=inA|inB;
 			3'b101:out=inA^inB;
-			3'b110:out=(inA + ~inB+1'b1)[3] ^ ((inA[3]!=inB[3])&&(inA+~inB+1)[3]!=inA[3]);
+			3'b110:out=(inA + ~inB+1'b1)[3] ^ ((inA[3]!=inB[3])&&(inA+~inB+1'b1)[3]!=inA[3]);
 			3'b111:out= ~|(inA +(~inB+1'b1));
 		endcase
 		
