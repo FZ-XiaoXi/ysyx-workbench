973e785f53d58f1c129ae5d33b026b7002d0cb7a
tracer-ysyx <tracer@ysyx.org>
2026-01-09 15:22:48 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:22:48 up  4:07,  1 user,  load average: 0.64, 1.16, 1.10

--- npc/vsrc/ALU.v
+++ npc/vsrc/ALU.v
@@ -1,17 +1,30 @@
 module ALU(
 	input cin,
-	input reg [3:0]inA,
-	input reg [3:0]inB,
-	input reg [2:0]mode,
-	output [3:0]out,
-	output CF,
-	output ZF,
-	output OF	
+	input [3:0]inA,
+	input [3:0]inB,
+	input [2:0]mode,
+	output reg [3:0]out,
+	output reg CF,
+	output reg ZF,
+	output reg OF	
 );
-	
-	assign CF=cin;
-	assign ZF= |inA;
-	assign OF= |mode;
-	assign out[3:0]=inB[3:0];
-
+	wire [3:0]inB_modified=cin?~inB:B;
+	wire [4:0]sum;
+	assign sum={1'b0,inA}+{1'b0,inB_modified}+{4'b0000,cin};
+	assign CF=cin?~sum[4]:sum[4];
+	assign OF=(inA[3]==inB_modified[3])&&(sum[3] != inA[3]);
+	always @(*)	begin
+		case(mode)
+			3'b000:out=sum[3:0];
+			3'b001:out=sum[3:0];
+			3'b010:out=~inA;
+			3'b011:out=inA&inB;
+			3'b100:out=inA|inB;
+			3'b101:out=inA^inB;
+			3'b110:out=(inA + ~inB+1)[3:3] ^ ((inA[3]!=inB[3])&&(inA+~inB+1)[3]!=inA[3]);
+			3'b111:out= ~|(inA +(~inB+1'b1));
+		endcase
+		
+	end
+	assign ZF= ~|out[3:0];
 endmodule
