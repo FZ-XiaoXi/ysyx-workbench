9141a51868501672b32bae414858eca0d740c9ab
tracer-ysyx <tracer@ysyx.org>
2026-01-10 14:06:52 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:06:52 up  5:11,  1 user,  load average: 2.05, 0.85, 0.64

---
+++ npc-exercise/P8/vsrc/vga.v
@@ -0,0 +1,20 @@
+module vga(
+	input clk,
+	input reset,
+	output vsync,
+	output hsync,
+	output valid,
+	output [7:0]vga_r,
+	output [7:0]vga_g,
+	output [7:0]vga_b
+);
+wire [9:0] v_addr;
+wire [9:0] h_addr;
+reg [23:0] vga_data;
+always @(*)begin
+	vga_data=24'hF00FFF;
+end
+wire pclk;
+clkgen #(25000000) my_vgaclk(clk,reset,1'b1,pclk);
+vga_ctrl u_vga_ctrl (pclk,reset,vga_data,h_addr,v_addr,hsync,vsync,valid,vga_r,vga_g,vga_b);
+endmodule
