f05c3b25e7c32eb75df458d0950ceb85f0ff3d11
tracer-ysyx <tracer@ysyx.org>
2026-01-10 13:23:42 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:23:42 up  4:28,  1 user,  load average: 0.42, 0.79, 0.57

--- npc-exercise/P7/vsrc/PS2.v
+++ npc-exercise/P7/vsrc/PS2.v
@@ -64,7 +64,7 @@ always @(posedge CLK)begin
 	end else begin
 		if(status==1)begin
 			ifPressed<=0;
-		end else if(status==0&&ready==1&&ifPressed==0)begin
+		end else if(status==0&&ready==1&&ifPressed==0&&nextstatus==0)begin
 			ifPressed<=1;
 			count<=count+1;
 		end
