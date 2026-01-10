546ee50307d07e801ed6aaf839b404531ee9b2b6
tracer-ysyx <tracer@ysyx.org>
2026-01-10 20:33:59 +0800

    > compile NEMU ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:33:59 up 11:39,  1 user,  load average: 1.13, 0.78, 0.73

--- nemu/src/isa/x86/include/isa-def.h
+++ nemu/src/isa/x86/include/isa-def.h
@@ -27,7 +27,10 @@
  */
 
 typedef struct {
+  
+
   /* Do NOT change the order of the GPRs' definitions. */
+  
   union{
     struct {
       union{
@@ -38,15 +41,16 @@ typedef struct {
     } gpr[8];
     union{
       struct
-      { //eax, ecx, edx, ebx, esp, ebp, esi, edi;
+      {
         uint32_t eax;
-        uint32_t ecx;
         uint32_t edx;
+        uint32_t ecx;
         uint32_t ebx;
-        uint32_t esp;
         uint32_t ebp;
         uint32_t esi;
         uint32_t edi;
+        uint32_t esp;
+        
       };
     };
   };
@@ -59,7 +63,7 @@ typedef struct {
   uint8_t *p_inst;
 } x86_ISADecodeInfo;
 
-enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
+enum { R_EAX, R_EDX, R_ECX, R_EBX, R_EBP, R_ESI, R_EDI, R_ESP };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
 enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
 
