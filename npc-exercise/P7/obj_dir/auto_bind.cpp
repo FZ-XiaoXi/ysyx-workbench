51c119a4f8ecbd92e02b0beae65a72c754edcbaf
tracer-ysyx <tracer@ysyx.org>
2026-01-10 11:27:10 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:27:10 up  2:32,  1 user,  load average: 0.56, 0.96, 0.67

--- npc-exercise/P7/obj_dir/auto_bind.cpp
+++ npc-exercise/P7/obj_dir/auto_bind.cpp
@@ -11,3 +11,8 @@ void nvboard_bind_all_pins(VPS2* top) {
 	nvboard_bind_pin( &top->seg1L, 8, SEG2A, SEG2B, SEG2C, SEG2D, SEG2E, SEG2F, SEG2G, DEC2P);
 	nvboard_bind_pin( &top->seg0H, 8, SEG1A, SEG1B, SEG1C, SEG1D, SEG1E, SEG1F, SEG1G, DEC1P);
 	nvboard_bind_pin( &top->seg0L, 8, SEG0A, SEG0B, SEG0C, SEG0D, SEG0E, SEG0F, SEG0G, DEC0P);
+	nvboard_bind_pin( &top->PS2_CLK, 1, PS2_CLK);
+	nvboard_bind_pin( &top->PS2_DAT, 1, PS2_DAT);
+	nvboard_bind_pin( &top->CLK, 1, SW0);
+	nvboard_bind_pin( &top->CLRN, 1, SW1);
+}
