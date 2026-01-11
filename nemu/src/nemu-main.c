7ae8a2606b06b5aea0135b02913d574dd2bb50e8
tracer-ysyx <tracer@ysyx.org>
2026-01-11 17:38:45 +0800

    > compile NEMU ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:38:45 up  8:16,  1 user,  load average: 1.30, 2.97, 3.71

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[]) {
   am_init_monitor();
 #else
   init_monitor(argc, argv);
-  char s[100]="((4) +10*((2- 1)*2))";
+  char s[100]="(4) +10*((2- 1)*2)";
   bool good=0;
   expr(s,&good);
 #endif
