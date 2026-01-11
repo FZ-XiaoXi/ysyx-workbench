7f8bfa365dbdb7dcc327617e87f9fbbf82b7b2d3
tracer-ysyx <tracer@ysyx.org>
2026-01-11 23:33:35 +0800

    > compile NEMU ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:33:35 up 14:11,  1 user,  load average: 6.30, 6.12, 4.94

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -234,7 +234,7 @@ uint32_t eval(int p, int q, bool *success) {
       case '-': return val1 - val2;
       case '*': return val1 * val2;
       case '/': return val1 / val2;
-      default: printf("Error OP: %c\n",tokens[op].type);*success=false;//assert(0);
+      default: printf("Error OP: %d\n",tokens[op].type);*success=false;//assert(0);
     }
   }
   //printf("NULL address!\n");
