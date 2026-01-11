ddc0df7b7f95f5250e698ccb204f61acfd537b29
tracer-ysyx <tracer@ysyx.org>
2026-01-11 11:54:25 +0800

    > compile NEMU ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:54:25 up  2:31,  1 user,  load average: 1.04, 0.80, 0.74

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,9 +24,12 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-  for(int i=0;0!=strcmp(regs[i],"s8");i++){
+  int i=0;
+  do{
     printf("%s\t%x\t%d\n",regs[i],isa_reg_str2val(regs[i],NULL),isa_reg_str2val(regs[i],NULL));
-  }
+    i++;
+  }while(0!=strcmp(regs[i],"t6"));
+    
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
