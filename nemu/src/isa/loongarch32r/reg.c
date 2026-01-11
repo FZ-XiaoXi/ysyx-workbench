20a94032d502c6ecdcc3667bf041717f45e9561e
tracer-ysyx <tracer@ysyx.org>
2026-01-11 11:40:57 +0800

    > compile NEMU ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:40:57 up  2:18,  1 user,  load average: 0.49, 0.73, 0.71

--- nemu/src/isa/loongarch32r/reg.c
+++ nemu/src/isa/loongarch32r/reg.c
@@ -24,7 +24,7 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-  for(int i=0;0!=strcmp(regs[i],"s8"),i++){
+  for(int i=0;0!=strcmp(regs[i],"s8");i++){
     printf("%s\t%x\t%ld\n",regs[i],isa_reg_str2val(regs[i],NULL),isa_reg_str2val(regs[i],NULL));
   }
 }
