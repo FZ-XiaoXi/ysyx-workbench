b665168817000d4480244cfff58355f8334b17c1
tracer-ysyx <tracer@ysyx.org>
2026-01-12 16:34:20 +0800

    > compile NEMU ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:20 up 1 day,  7:11,  1 user,  load average: 1.56, 1.21, 1.13

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -49,7 +49,7 @@ void test_args(){
         bool success=true;
         count++;
         //printf("Get[%d]!\t%u\t%s\n",count,ans,s);
-        printf("%s\n",s);
+        //printf("%s\n",s);
         val=expr(s,&success);
         if(success==true){
           if(val==ans) printf("PASS[%d]\n",count);
