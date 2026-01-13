14a6a46cb70290fcecde633526d24a4370fdc352
tracer-ysyx <tracer@ysyx.org>
2026-01-13 13:34:25 +0800

    > compile NEMU ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:34:25 up 2 days,  4:11,  1 user,  load average: 0.80, 0.67, 0.76

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -39,9 +39,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 #ifdef CONFIG_WATCHPOINT
-printf("aaa\n");
   for(int i=0;i<32;i++){
-    
     if(1==diff_wp(i)){
       WP *wp=find_wp(i);
       bool success=true;
