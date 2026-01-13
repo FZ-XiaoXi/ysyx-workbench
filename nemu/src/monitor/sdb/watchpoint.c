befb031ddce6b00e0150e6fb1bb5ea7f1ea3f0ec
tracer-ysyx <tracer@ysyx.org>
2026-01-13 13:12:33 +0800

    > compile NEMU ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:12:33 up 2 days,  3:49,  1 user,  load average: 1.69, 1.24, 1.21

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -94,7 +94,9 @@ void print_wp(){
   printf("id\tvalue\texp\n");
   while(1){
     if(wp==NULL)  break;
-    printf("%d\t%u\t%u\t%s\n",wp->NO,expr(wp->expr,&success),wp->val,wp->expr);
+    uint32_t val=expr(wp->expr,&success);
+    if(success==false) printf("Error experiment!\n");
+    printf("%d\t%u\t%u\t%s\n",wp->NO,val,wp->val,wp->expr);
     wp=wp->next;
   }
   wp=free_;
