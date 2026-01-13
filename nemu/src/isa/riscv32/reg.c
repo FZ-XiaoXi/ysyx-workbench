ec4f4d6fd8ccb387ade3e3dba49d03857092668c
tracer-ysyx <tracer@ysyx.org>
2026-01-13 14:02:17 +0800

    > compile NEMU ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:02:17 up 2 days,  4:39,  1 user,  load average: 0.92, 1.08, 1.04

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -39,6 +39,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   for(int i=0;i<32;i++){
     if(strcmp(s,regs[i])==0)  return cpu.gpr[i];
   }
+  if(strcmp(s,"pc")==0) return cpu.pc;
   *success=false;
   return 0;
 }
