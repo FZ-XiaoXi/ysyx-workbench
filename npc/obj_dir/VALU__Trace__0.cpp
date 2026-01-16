87c6a207e993774ce951d8e270116c7feb7b6f71
tracer-ysyx <tracer@ysyx.org>
2026-01-16 14:50:28 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:50:28 up  2:12,  1 user,  load average: 0.82, 0.44, 0.28

---
+++ npc/obj_dir/VALU__Trace__0.cpp
@@ -0,0 +1,47 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Tracing implementation internals
+#include "verilated_vcd_c.h"
+#include "VALU__Syms.h"
+
+
+void VALU___024root__trace_chg_0_sub_0(VALU___024root* vlSelf, VerilatedVcd::Buffer* bufp);
+
+void VALU___024root__trace_chg_0(void* voidSelf, VerilatedVcd::Buffer* bufp) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VALU___024root__trace_chg_0\n"); );
+    // Body
+    VALU___024root* const __restrict vlSelf VL_ATTR_UNUSED = static_cast<VALU___024root*>(voidSelf);
+    VALU__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    if (VL_UNLIKELY(!vlSymsp->__Vm_activity)) return;
+    VALU___024root__trace_chg_0_sub_0((&vlSymsp->TOP), bufp);
+}
+
+void VALU___024root__trace_chg_0_sub_0(VALU___024root* vlSelf, VerilatedVcd::Buffer* bufp) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VALU___024root__trace_chg_0_sub_0\n"); );
+    VALU__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    auto& vlSelfRef = std::ref(*vlSelf).get();
+    // Body
+    uint32_t* const oldp VL_ATTR_UNUSED = bufp->oldp(vlSymsp->__Vm_baseCode + 1);
+    if (VL_UNLIKELY((vlSelfRef.__Vm_traceActivity[1U]))) {
+        bufp->chgCData(oldp+0,(vlSelfRef.ALU__DOT__inB_modified),4);
+        bufp->chgCData(oldp+1,(vlSelfRef.ALU__DOT__sum),5);
+        bufp->chgCData(oldp+2,(vlSelfRef.ALU__DOT__diff),4);
+    }
+    bufp->chgCData(oldp+3,(vlSelfRef.inA),4);
+    bufp->chgCData(oldp+4,(vlSelfRef.inB),4);
+    bufp->chgCData(oldp+5,(vlSelfRef.mode),3);
+    bufp->chgCData(oldp+6,(vlSelfRef.out),4);
+    bufp->chgBit(oldp+7,(vlSelfRef.CF));
+    bufp->chgBit(oldp+8,(vlSelfRef.ZF));
+    bufp->chgBit(oldp+9,(vlSelfRef.OF));
+    bufp->chgBit(oldp+10,((1U == (IData)(vlSelfRef.mode))));
+}
+
+void VALU___024root__trace_cleanup(void* voidSelf, VerilatedVcd* /*unused*/) {
+    VL_DEBUG_IF(VL_DBG_MSGF("+    VALU___024root__trace_cleanup\n"); );
+    // Body
+    VALU___024root* const __restrict vlSelf VL_ATTR_UNUSED = static_cast<VALU___024root*>(voidSelf);
+    VALU__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    vlSymsp->__Vm_activity = false;
+    vlSymsp->TOP.__Vm_traceActivity[0U] = 0U;
+    vlSymsp->TOP.__Vm_traceActivity[1U] = 0U;
+}
