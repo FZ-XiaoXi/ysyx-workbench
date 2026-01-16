47bffa1ce0675f0abf6c5802a7cc5d99254af4b8
tracer-ysyx <tracer@ysyx.org>
2026-01-16 21:22:32 +0800

    > sim RTL ysyx_22040000 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:32 up  8:44,  1 user,  load average: 1.02, 0.78, 0.50

---
+++ npc-exercise/sCPU/obj_dir/VsCPU___024root__Slow.cpp
@@ -0,0 +1,22 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Design implementation internals
+// See VsCPU.h for the primary calling header
+
+#include "VsCPU__pch.h"
+
+void VsCPU___024root___ctor_var_reset(VsCPU___024root* vlSelf);
+
+VsCPU___024root::VsCPU___024root(VsCPU__Syms* symsp, const char* v__name)
+    : VerilatedModule{v__name}
+    , vlSymsp{symsp}
+ {
+    // Reset structure values
+    VsCPU___024root___ctor_var_reset(this);
+}
+
+void VsCPU___024root::__Vconfigure(bool first) {
+    (void)first;  // Prevent unused variable warning
+}
+
+VsCPU___024root::~VsCPU___024root() {
+}
