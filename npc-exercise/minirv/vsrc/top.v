a2e96ab8f34d12698c78e70d670ea4d60e56773e
tracer-ysyx <tracer@ysyx.org>
2026-02-05 13:46:49 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:46:49 up  3:22,  1 user,  load average: 6.51, 6.98, 7.60

--- npc-exercise/minirv/vsrc/top.v
+++ npc-exercise/minirv/vsrc/top.v
@@ -1,13 +1,9 @@
 module top(
   input clk,
-  input rst,
-  output clk0,
-  output clk1,
-  output clk2,
-  output [2:0] cnt
+  input rst
 );
-//wire clk0,clk1,clk2;
-clkdiv clkdiv_0(clk,rst,clk0,clk1,clk2,cnt);
+wire clk0,clk1,clk2;
+clkdiv clkdiv_0(clk,rst,clk0,clk1,clk2);
   
 endmodule
 
@@ -17,13 +13,13 @@ module clkdiv(
   input rst,
   output clk0,
   output clk1,
-  output clk2,
-  output reg [2:0] cnt
+  output clk2
 );
-  
+  reg [2:0] cnt;
   always @(posedge clk,posedge rst) begin
     if(rst) cnt<=3'b001;
     else cnt<={cnt[1:0],cnt[2:2]};
+    $display("clkdiv: cnt=%b, clk0=%b, clk1=%b, clk2=%b", cnt, clk0, clk1, clk2);
   end
   assign clk0=cnt[0];
   assign clk1=cnt[1];
