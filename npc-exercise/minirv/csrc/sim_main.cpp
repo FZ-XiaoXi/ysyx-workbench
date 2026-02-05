300437f3dab03858ab8da78e562f0ca9123723ef
tracer-ysyx <tracer@ysyx.org>
2026-02-05 12:54:39 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:54:39 up  2:29,  1 user,  load average: 7.41, 7.39, 7.24

---
+++ npc-exercise/minirv/csrc/sim_main.cpp
@@ -0,0 +1,33 @@
+#include "Vtop.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <assert.h>
+#include "verilated.h"
+
+#define MAX_PC 0xfffffff
+#define pmem_read(pc) MEM[pc>>2]
+uint32_t MEM[MAX_PC];
+
+
+int main(int argc, char** argv) {
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vtop* top = new Vtop{contextp};
+	top->clk=0;
+	top->rst=1;
+	contextp->timeInc(10);
+	top->clk=0;
+	top->rst=0;
+	contextp->timeInc(10);
+	while (!contextp->gotFinish()) {
+		top->clk=!top->clk;
+		top->eval();
+		printf("%d,%d,%d",top->clk0,top->clk1,top->clk2);
+		contextp->timeInc(5);
+	}
+    delete top;
+    delete contextp;
+    return 0;
+}
+
---
+++ npc-exercise/minirv/vsrc/top.v
@@ -0,0 +1,29 @@
+module top(
+  input clk,
+  input rst,
+  output clk0,
+  output clk1,
+  output clk2
+);
+//wire clk0,clk1,clk2;
+clkdiv clkdiv_0(clk,rst,clk0,clk1,clk2);
+  
+endmodule
+
+
+module clkdiv(
+  input clk,
+  input rst,
+  output clk0,
+  output clk1,
+  output clk2
+);
+  reg [2:0] cnt;
+  always @(posedge clk,posedge rst) begin
+    if(rst) cnt<=3'b001;
+    else cnt<={cnt[1:0],cnt[2:2]};
+  end
+  assign clk0=cnt[0];
+  assign clk1=cnt[1];
+  assign clk2=cnt[2];
+endmodule
