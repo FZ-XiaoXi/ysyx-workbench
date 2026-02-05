586cc0a3827fa419dafc387db1436978429a5056
tracer-ysyx <tracer@ysyx.org>
2026-02-06 01:00:59 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:00:59 up 14:36,  1 user,  load average: 6.78, 6.90, 6.90

--- npc-exercise/minirv/vsrc/IDU.v
+++ npc-exercise/minirv/vsrc/IDU.v
@@ -46,11 +46,11 @@ module IDU(
     assign isJ=(0)?1:0;
 
     always @(*) begin
-        if     (isI)    imm={20{immI[11:11]},immI[11:0]};
-        else if(isJ)    imm={11{immJ[20:20]},immJ[20:1],1'b0};
-        else if(isS)    imm={20{immS[11:11]},immS[11:0]};
-        else if(isU)    imm={  {immU[31:12]},12{1'b0}};
-        else if(isB)    imm={19{immB[12:12]},immB[12:1],1'b0};
+        if     (isI)    imm={{20{immI[11:11]}},immI[11:0]};
+        else if(isJ)    imm={{11{immJ[20:20]}},immJ[20:1],1'b0};
+        else if(isS)    imm={{20{immS[11:11]}},immS[11:0]};
+        else if(isU)    imm={   {immU[31:12]} ,12{1'b0}};
+        else if(isB)    imm={{19{immB[12:12]}},immB[12:1],1'b0};
         else imm=0;
 
     end
