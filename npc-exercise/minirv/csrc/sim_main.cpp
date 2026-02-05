76bf563608efb8149933bacf39ced54940a3acc7
tracer-ysyx <tracer@ysyx.org>
2026-02-06 01:38:19 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:38:19 up 15:13,  1 user,  load average: 7.04, 7.19, 7.11

--- npc-exercise/minirv/csrc/sim_main.cpp
+++ npc-exercise/minirv/csrc/sim_main.cpp
@@ -46,6 +46,7 @@ int main(int argc, char** argv) {
 		mem_write(top->clk,top->LSU_address,top->LSU_writedata,top->LSU_range,top->LSU_WEN);
 		printf("%d PC:%x CMD:%x | ",top->clk,top->PC,top->PC_command);
 		for(int i=0;i<16;i++) printf("R[%d]:%d ",i,top->GPRTEST[i]);
+		printf("\n");
 		top->clk=!top->clk;
 		top->eval();
 		
