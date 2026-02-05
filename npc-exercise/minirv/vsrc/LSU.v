e58daf417ef9c8239e3c0e10899954b9d3918bbb
tracer-ysyx <tracer@ysyx.org>
2026-02-06 01:02:13 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:02:13 up 14:37,  1 user,  load average: 7.21, 7.00, 6.93

--- npc-exercise/minirv/vsrc/LSU.v
+++ npc-exercise/minirv/vsrc/LSU.v
@@ -1,9 +1,9 @@
 module LSU(
     input [31:0]address,
-    output [31:0]data
+    output [31:0]data,
     input [31:0]PC_address,
     output [31:0]PC_data,
-    
+
     input [31:0]wdata,
     input [3:0]range,
     input clk,
