4b4fc29761d06f02a1d7616b83829c7aab859ebb
tracer-ysyx <tracer@ysyx.org>
2026-02-05 13:02:18 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:02:18 up  2:37,  1 user,  load average: 7.21, 7.36, 7.29

--- npc-exercise/minirv/vsrc/top.v
+++ npc-exercise/minirv/vsrc/top.v
@@ -1,11 +1,26 @@
+module top(
+  input clk,
+  input rst,
+  output clk0,
+  output clk1,
+  output clk2,
+  output [2:0] cnt;
+);
+//wire clk0,clk1,clk2;
+clkdiv clkdiv_0(clk,rst,clk0,clk1,cnt);
+  
+endmodule
+
+
 module clkdiv(
   input clk,
   input rst,
   output clk0,
   output clk1,
-  output clk2
+  output clk2,
+  output reg [2:0] cnt;
 );
-  reg [2:0] cnt;
+  
   always @(posedge clk,posedge rst) begin
     if(rst) cnt<=3'b001;
     else cnt<={cnt[1:0],cnt[2:2]};
@@ -14,16 +29,3 @@ module clkdiv(
   assign clk1=cnt[1];
   assign clk2=cnt[2];
 endmodule
-module top(
-  input clk,
-  input rst,
-  output clk0,
-  output clk1,
-  output clk2
-);
-//wire clk0,clk1,clk2;
-clkdiv clkdiv_0(clk,rst,clk0,clk1,clk2);
-  
-endmodule
-
-
