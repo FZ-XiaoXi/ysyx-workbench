6bbaf8fead60ad64ac1f026ecadcb9a92fb27c7a
tracer-ysyx <tracer@ysyx.org>
2026-02-06 01:27:33 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:27:33 up 15:02,  1 user,  load average: 7.14, 7.04, 6.92

--- npc-exercise/minirv/csrc/sim_main.cpp
+++ npc-exercise/minirv/csrc/sim_main.cpp
@@ -35,17 +35,16 @@ int main(int argc, char** argv) {
 	top->rst=0;
 	top->eval();
 	contextp->timeInc(10);
-	
-	while(1)printf("ss\n");;
-	// while (!contextp->gotFinish()) {
-	// 	top->PC_command=mem_read(top->PC,top->LSU_range);
-	// 	top->LSU_readdata=mem_read(top->LSU_address,top->LSU_range);
-	// 	mem_write(top->clk,top->LSU_address,top->LSU_writedata,top->LSU_range,top->LSU_WEN);
-	// 	top->clk=!top->clk;
-	// 	top->eval();
+	while (!contextp->gotFinish()) {
+		top->PC_command=mem_read(top->PC,top->LSU_range);
+		top->LSU_readdata=mem_read(top->LSU_address,top->LSU_range);
+		mem_write(top->clk,top->LSU_address,top->LSU_writedata,top->LSU_range,top->LSU_WEN);
+		printf("%d PC:%x CMD:%x\n",top->clk,top->PC,top->PC_command);
+		top->clk=!top->clk;
+		top->eval();
 		
-	// 	contextp->timeInc(5);
-	// }
+		contextp->timeInc(5);
+	}
     delete top;
     delete contextp;
     return 0;
