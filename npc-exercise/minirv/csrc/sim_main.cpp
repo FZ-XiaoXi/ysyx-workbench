be9f00cea799a9e3816a1c0205e9085b9eaf0fa9
tracer-ysyx <tracer@ysyx.org>
2026-02-06 18:30:22 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:30:22 up 1 day,  8:05,  1 user,  load average: 6.82, 6.93, 6.99

--- npc-exercise/minirv/csrc/sim_main.cpp
+++ npc-exercise/minirv/csrc/sim_main.cpp
@@ -8,7 +8,7 @@
 #include "verilated.h"
 #define READ 1
 #define FILE_NAME "mem.bin"
-#define MAX_MEM 0xfffffff
+#define MAX_MEM 0xaffffff
 #define _EBREAK 0b00000000000100000000000001110011
 uint32_t MEM[MAX_MEM];
 int isEBREAK=0;
