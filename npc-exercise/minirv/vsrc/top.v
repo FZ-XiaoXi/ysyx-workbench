35a4c3922ab9abad0b9d57f76f475309fbdc409b
tracer-ysyx <tracer@ysyx.org>
2026-02-06 15:03:02 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:03:02 up 1 day,  4:38,  1 user,  load average: 7.02, 6.95, 6.93

--- npc-exercise/minirv/vsrc/top.v
+++ npc-exercise/minirv/vsrc/top.v
@@ -1,6 +1,6 @@
 import "DPI-C" function void ebreak();
-
-
+import "DPI-C" function int pmem_read(input int raddr);
+import "DPI-C" function void pmem_write(input int waddr, input int wdata, input byte wmask);
 
 module top(
   input clk,
