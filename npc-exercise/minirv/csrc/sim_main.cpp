87767a3c03ffeaad948b3aae51488b8174e14eba
tracer-ysyx <tracer@ysyx.org>
2026-02-06 18:17:59 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:17:59 up 1 day,  7:53,  1 user,  load average: 7.41, 7.20, 7.01

--- npc-exercise/minirv/csrc/sim_main.cpp
+++ npc-exercise/minirv/csrc/sim_main.cpp
@@ -18,9 +18,9 @@ uint32_t sPC=0;
 int pmem_read(int raddr){
 	raddr=raddr & MAX_MEM;
 	// 总是读取地址为`raddr & ~0x3u`的4字节返回
-	printf("\nR: add:%x ",raddr);
+	//printf("\nR: add:%x ",raddr);
 	uint32_t s=MEM[raddr>>2];
-	printf("val:%x\n",raddr,s);
+	//printf("val:%x\n",raddr,s);
 	return s;
 }
 void pmem_write(int waddr, int wdata, char wmask) {
