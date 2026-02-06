abf56ec7f80f4032e2fc3312d7773403dbf54755
tracer-ysyx <tracer@ysyx.org>
2026-02-06 18:10:11 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:10:11 up 1 day,  7:45,  1 user,  load average: 6.80, 6.90, 6.91

--- npc-exercise/minirv/obj_dir/Vtop.mk
+++ npc-exercise/minirv/obj_dir/Vtop.mk
@@ -38,6 +38,7 @@ VM_MODPREFIX = Vtop
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
   -DTOP_NAME="Vtop" \
+  -ggdb \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
