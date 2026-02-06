7b955d38ea928525797c6a9cc66ccf08995b8cfa
tracer-ysyx <tracer@ysyx.org>
2026-02-06 15:32:03 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:32:03 up 1 day,  5:07,  1 user,  load average: 6.74, 6.85, 6.90

--- npc-exercise/minirv/csrc/sim_main.cpp
+++ npc-exercise/minirv/csrc/sim_main.cpp
@@ -65,9 +65,10 @@ void onecyc(VerilatedContext* contextp,Vtop* top){
 	top->eval();
 	contextp->timeInc(5);
 	
-	printf("PC:%04x ",sPC);
-	printf("CMD:%08x ↘️ ",top->PC_command);
+	
 	for(int i=0;i<16;i++) printf("[%2d]:%04x ",i,top->GPRTEST[i]);
+	printf("| PC:%04x ",top->PC);
+	printf("CMD:%08x",top->PC_command);
 	printf("\n");
 }
 
