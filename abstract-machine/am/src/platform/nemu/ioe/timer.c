a61933e633753b6867a103f67e42f2aeaddd84ce
tracer-ysyx <tracer@ysyx.org>
2026-02-07 16:15:44 +0800

    > compile NEMU ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:15:44 up 36 min,  1 user,  load average: 7.01, 7.39, 6.64

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -6,7 +6,7 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = inl(RTC_ADDR);
-  uptime->us += (uint64_t)inl(RTC_ADDR+2) << 32;
+  uptime->us += (uint64_t)inl(RTC_ADDR+1) << 32;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
