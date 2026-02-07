391d24e9fa861361158cb4f74b3d82e4814d5797
tracer-ysyx <tracer@ysyx.org>
2026-02-07 15:37:40 +0800

    > compile NEMU ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:37:40 up 2 days,  5:12,  1 user,  load average: 7.15, 7.08, 7.04

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -42,7 +42,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
         case 'd':
           itoa(va_arg(ap, int),ds,10);
 					int len=strlen(ds);
-					printf("===%d===%d===\n",width,len);
+					putch(width+('0'-width));
 					if(width<=len){
 						int i=0;
 						while(ds[i]!='\0') *(out++)=ds[i++];
