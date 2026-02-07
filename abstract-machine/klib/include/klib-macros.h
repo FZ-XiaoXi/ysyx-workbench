6fcd22573a4c1a68b705f2a9a79a420ceacc328d
tracer-ysyx <tracer@ysyx.org>
2026-02-07 22:38:17 +0800

    > compile NEMU ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:38:17 up  6:59,  1 user,  load average: 7.29, 7.22, 7.19

--- abstract-machine/klib/include/klib-macros.h
+++ abstract-machine/klib/include/klib-macros.h
@@ -19,7 +19,7 @@
   ({ reg##_T __io_param; \
     ioe_read(reg, &__io_param); \
     __io_param; })
-
+//({ reg##_T __io_param; ioe_read(reg, &__io_param); __io_param; })
 #define io_write(reg, ...) \
   ({ reg##_T __io_param = (reg##_T) { __VA_ARGS__ }; \
     ioe_write(reg, &__io_param); })
