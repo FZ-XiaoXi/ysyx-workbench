45dc4bf37cb90d84ef49d222dd6b8e5f82f2ba5c
tracer-ysyx <tracer@ysyx.org>
2026-02-09 00:30:13 +0800

    > compile NEMU ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:30:13 up 13:56,  1 user,  load average: 6.90, 6.59, 6.56

--- nemu/include/utils.h
+++ nemu/include/utils.h
@@ -58,10 +58,10 @@ uint64_t get_time();
 
 #define log_write(...) IFDEF(CONFIG_TARGET_NATIVE_ELF, \
   do { \
-    printf("S");\
     extern FILE* log_fp; \
     extern bool log_enable(); \
     if (log_enable() && log_fp != NULL) { \
+      printf("W");\
       fprintf(log_fp, __VA_ARGS__); \
       fflush(log_fp); \
     } \
