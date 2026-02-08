3dafa8ee69903539e40700b33473beb95d4f0460
tracer-ysyx <tracer@ysyx.org>
2026-02-08 23:37:19 +0800

    > compile NEMU ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:37:19 up 13:04,  1 user,  load average: 7.16, 6.97, 6.86

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -97,9 +97,9 @@ static void load_elf(){
   Log("ELF SECTION .symtab OFFSET: 0x%x num:%d",elf_section_symtab_off,elf_section_symtab_num);
   
   //Set symtab
-  Elf32_Sym *elf_section_symtab=(Elf32_Sym*)(elf_header+elf_section_symtab_off);
+  //Elf32_Sym *elf_section_symtab=(Elf32_Sym*)(elf_header+elf_section_symtab_off);
   for(int i=1;i<elf_section_symtab_num;i++){
-    printf("===%d===\n",elf_section_header[elf_section_header[elf_section_symtab_index].sh_link].sh_offset + elf_section_symtab[i].st_name);
+    printf("===%d===\n",elf_section_header[elf_section_symtab_index].sh_link);
     //strcpy(symtab[i].name,(char *)(elf_buf + elf_section_header[elf_section_header[elf_section_symtab_index].sh_link].sh_offset + elf_section_symtab[i].st_name));
     //symtab[i].start_add=elf_section_symtab[i].st_value;
     //symtab[i].end_add=elf_section_symtab[i].st_value + elf_section_symtab[i].st_size;
