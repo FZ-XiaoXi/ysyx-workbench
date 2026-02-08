788eebf88a67df212e4a7519a2fd4775513983ff
tracer-ysyx <tracer@ysyx.org>
2026-02-08 17:08:26 +0800

    > compile NEMU ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:08:26 up  6:35,  1 user,  load average: 6.39, 6.70, 6.73

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -36,7 +36,7 @@ void print_ring_inst_buf(){
   printf("Instruction ring tracer:\n");
   for(int i=0;i<CONFIG_ITRACE_RING_MAX;i++){
     if(ring_inst_buf[i][0]=='\0') continue;
-    printf("=%s=\n",ring_inst_buf[i]);
+    printf("=%s\t\t\t=\n",ring_inst_buf[i]);
   }
 }
 #endif
