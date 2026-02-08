99f43b8ccb5756bcb1204ceb76f750b700233bb3
tracer-ysyx <tracer@ysyx.org>
2026-02-08 22:32:03 +0800

    > compile NEMU ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:32:03 up 11:58,  1 user,  load average: 7.05, 6.94, 6.73

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -77,13 +77,21 @@ static void load_elf(){
   Log("ELF SECTION HEADER OFFSET: %d, NUM: %d, SIZE: %d",elf_header->e_shoff,elf_header->e_shnum,elf_header->e_shentsize);
 
   //Get elf_section_header_strtab
-  Elf32_Off elf_section_header_strtab=elf_section_headers[elf_header->e_shstrndx].sh_offset;
+  Elf32_Off elf_section_header_strtab_off=elf_section_headers[elf_header->e_shstrndx].sh_offset;
+  Elf32_Off elf_section_symtab_off=0;
+  Elf32_Off elf_section_strtab_off=0;
   for(int i=0;i<elf_header->e_shnum;i++){
-    if(strcmp(elf_buf+elf_section_header_strtab+elf_section_headers[i].sh_name,".symtab")==0){
-      Log("================%x===============",elf_section_headers[i].sh_offset);
+    if(strcmp(elf_buf+elf_section_header_strtab_off+elf_section_headers[i].sh_name,".symtab")==0){
+      elf_section_symtab_off=elf_section_headers[i].sh_offset;
+    }else if(strcmp(elf_buf+elf_section_header_strtab_off+elf_section_headers[i].sh_name,".strtab")==0){
+      elf_section_strtab_off=elf_section_headers[i].sh_offset;
     }
-
   }
+  if(elf_section_symtab_off==0){free(elf_buf);free(elf_header);free(elf_section_headers);Log("Cannot init 'ftrace'. (find .symtab ERROR) Disabled 'ftrace'.");return;}
+  if(elf_section_strtab_off==0){free(elf_buf);free(elf_header);free(elf_section_headers);Log("Cannot init 'ftrace'. (find .symtab ERROR) Disabled 'ftrace'.");return;}
+  Log("ELF SECTION .strtab OFFSET: %d",elf_section_strtab_off);
+  Log("ELF SECTION .symtab OFFSET: %d",elf_section_symtab_off);
+
   free(elf_section_headers);
   free(elf_header);
   free(elf_buf);
