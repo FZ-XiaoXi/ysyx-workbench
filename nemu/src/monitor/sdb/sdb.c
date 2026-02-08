0f16740315cffd37ab29a038d735f863a6362b65
tracer-ysyx <tracer@ysyx.org>
2026-02-08 17:59:10 +0800

    > compile NEMU ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:59:10 up  7:25,  1 user,  load average: 6.60, 6.61, 6.71

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -115,7 +115,8 @@ static int cmd_x(char *args) {
     }
     printf("%x\n",val);
     for(int i=0;i<num;i++){
-      printf("%.4d\t0x%.8x\t0x%.8x\t%d\n",i,val+i*4,vaddr_read((vaddr_t)val+i*4,4),vaddr_read((vaddr_t)val+i*4,4));
+      uint32_t data=vaddr_read((vaddr_t)val+i*4,4);
+      printf("%.4d\t0x%.8x\t0x%.8x\t%d\n",i,val+i*4,data,data);
     }
     
     return 0;
