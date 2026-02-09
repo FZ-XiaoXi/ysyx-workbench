f2a6634f9e4e415a6fed410c56091df7e8fc5eb5
tracer-ysyx <tracer@ysyx.org>
2026-02-09 15:10:12 +0800

    > compile NEMU ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:10:12 up 1 day,  4:36,  1 user,  load average: 6.46, 6.63, 6.67

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -29,10 +29,10 @@ static void print_devtrace(dtrace_t op,paddr_t addr,uint32_t val,IOMap *map){
   char buf[128]={0};
   if(op==DEVREAD){
     sprintf(buf,"DEVTracer: READ device [%s] [0x%08x] = '0x%08x' at pc = '0x%08x'",map->name,addr,val,cpu.pc);
-    Log("DEVTracer: READ device [%s] [0x%08x] = '0x%08x' at pc = '0x%08x'",map->name,addr,val,cpu.pc);
+    //Log("DEVTracer: READ device [%s] [0x%08x] = '0x%08x' at pc = '0x%08x'",map->name,addr,val,cpu.pc);
   }else if(op==DEVWRITE){
     sprintf(buf,"DEVTracer: WRITE device [%s] [0x%08x] = '0x%08x' at pc = '0x%08x'",map->name,addr,val,cpu.pc);
-    Log("DEVTracer: WRITE device [%s] [0x%08x] = '0x%08x' at pc = '0x%08x'",map->name,addr,val,cpu.pc);
+    //Log("DEVTracer: WRITE device [%s] [0x%08x] = '0x%08x' at pc = '0x%08x'",map->name,addr,val,cpu.pc);
   }
   log_write("%s\n", buf);
 #endif
