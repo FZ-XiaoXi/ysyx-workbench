23937aa032f4b8e0f0cee95bad6539187c8beb1b
tracer-ysyx <tracer@ysyx.org>
2026-02-09 13:09:41 +0800

    > compile NEMU ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:09:41 up 1 day,  2:36,  1 user,  load average: 6.64, 6.58, 6.65

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -237,6 +237,7 @@ void ftracer_write_log(char *s){
     ftrace_log.buf=malloc(128);
     if(!ftrace_log.buf){Log("ERROR first malloc ftracer_log_buffer!.");return;}
     memset(ftrace_log.buf,0,128);
+    ftrace_log.alloc=128;
   }
   while(strlen(s)>ftrace_log.alloc-ftrace_log.len-1){
     char *tpr=realloc(ftrace_log.buf,ftrace_log.alloc+128);
