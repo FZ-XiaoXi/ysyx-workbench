0874e2ad196069423284d1f06bf0af4fc55c5bce
tracer-ysyx <tracer@ysyx.org>
2026-02-09 17:44:35 +0800

    > compile NEMU ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:44:35 up 1 day,  7:11,  1 user,  load average: 6.55, 6.62, 6.62

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,10 +3,14 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+static int gpu_w,gpu_h;
+
 void __am_gpu_init() {
   int i;
   int w = (unsigned int)inl(VGACTL_ADDR)>>16;  // TODO: get the correct width
   int h = (unsigned int)inl(VGACTL_ADDR)&0xffff;  // TODO: get the correct height
+  gpu_w = w;
+  gpu_h = h;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
@@ -16,13 +20,23 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = (unsigned int)inl(VGACTL_ADDR)>>16, .height = (unsigned int)inl(VGACTL_ADDR)&0xffff,
-    .vmemsz = inl(FB_ADDR)
+    .vmemsz = gpu_w*gpu_h*4
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
+  if (ctl->sync) outl(SYNC_ADDR, 1);
+  if(ctl->pixels == NULL) return;
+  int index=0;
+  for(int i=0;i<gpu_w * gpu_h;i++){
+    int x=i%gpu_w;
+    int y=i/gpu_h;
+    if(x>=ctl->x && x < ctl->x+ctl->w){
+      if(y>=ctl->y && y<ctl->y+ctl->h){
+        outl(FB_ADDR,*((unsigned int*)ctl->pixels + index));
+        index++;
+      }
+    }
   }
 }
 
