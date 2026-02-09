fca7b83ee6751639bf101123c0b21306ed660b25
tracer-ysyx <tracer@ysyx.org>
2026-02-09 15:30:25 +0800

    > compile NEMU ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:30:25 up 1 day,  4:57,  1 user,  load average: 6.49, 6.35, 6.44

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,10 +1,12 @@
 #include <am.h>
 #include <nemu.h>
+#include <klib.h>
 
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   int code = inl(KBD_ADDR);
+  printf("CODE=%d",code);
   kbd->keydown = (code!=AM_KEY_NONE)?true:false;
   kbd->keycode = code;
 }
