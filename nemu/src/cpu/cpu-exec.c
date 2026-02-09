10799281f8c28cc21f8514b1ae15ae248ef18807
tracer-ysyx <tracer@ysyx.org>
2026-02-09 11:21:48 +0800

    > compile NEMU ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:21:48 up 1 day, 48 min,  1 user,  load average: 6.50, 6.66, 6.74

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -175,7 +175,6 @@ extern ftracer_stack_t  ftracer_stack;
 void func_trace(Decode *s){
   if(!funsymtab || !ftracer_stack.is_ftrace)  return;
   for(int i=0;i<ftracer_stack.symtab_size;i++){
-    
     if(s->dnpc == funsymtab[i].start_add){
       ftracer_t stack_frame = {.dst_func = funsymtab + i, .dst_pc = s->dnpc, .src_pc = s->pc};
       ftracer_push(stack_frame);
--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -111,7 +111,7 @@ static void load_elf(){
   
   //init ftracer stack
   ftracer_stack.is_ftrace=true;
-  ftracer_stack.symtab_size=elf_section_symtab_num;
+  ftracer_stack.symtab_size=cnt;
   for(int i=0;i<ftracer_stack.symtab_size;i++){
     if(funsymtab[i].start_add == RESET_VECTOR){
       ftracer_t stack_frame = {.dst_func = funsymtab + i, .dst_pc = RESET_VECTOR, .src_pc = RESET_VECTOR};
