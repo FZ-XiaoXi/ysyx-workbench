a1d6714f358bd3727c0ad2f281c165a8c5d89c60
FZ_XiaoXi <FZXiaoxi@outlook.com>
2026-02-11 13:51:09 +0800

    FINAL: NEMU-AUDIO(TARGET=AM)

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -1,6 +1,6 @@
 #include <am.h>
 #include <nemu.h>
-
+#include <klib.h>
 #define AUDIO_FREQ_ADDR      (AUDIO_ADDR + 0x00)
 #define AUDIO_CHANNELS_ADDR  (AUDIO_ADDR + 0x04)
 #define AUDIO_SAMPLES_ADDR   (AUDIO_ADDR + 0x08)
@@ -8,19 +8,38 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
+int sbuf_size=0;
 void __am_audio_init() {
+  outl(AUDIO_INIT_ADDR,1);
+  sbuf_size=inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = inl(AUDIO_INIT_ADDR);
+  cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+  outl(AUDIO_FREQ_ADDR,ctrl->freq);
+  outl(AUDIO_CHANNELS_ADDR,ctrl->channels);
+  outl(AUDIO_SAMPLES_ADDR,ctrl->samples);
+  printf("SSSSST\n");
+  outl(AUDIO_INIT_ADDR,0);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  int i = 0;
+  int c=inl(AUDIO_COUNT_ADDR);
+  //printf("Get COUNT=%d\n",c);
+  int size=ctl->buf.end - ctl->buf.start;
+  if(c + size > sbuf_size) size = sbuf_size - c;
+  while(i<size){
+    outb(AUDIO_SBUF_ADDR + c + i,*((char *)ctl->buf.start + i));
+    i++;
+  }
+  outl(AUDIO_COUNT_ADDR, c + i);
 }
