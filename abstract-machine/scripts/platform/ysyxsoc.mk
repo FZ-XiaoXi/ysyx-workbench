d299177747bc53905a7becdf748e81d5ad329d5b
FZ_XiaoXi <FZXiaoxi@outlook.com>
2026-08-12 11:36:22 +0800

    修改objcopy指令 跑起RTT

--- abstract-machine/scripts/platform/ysyxsoc.mk
+++ abstract-machine/scripts/platform/ysyxsoc.mk
@@ -29,7 +29,7 @@ insert-arg: image
 image: image-dep
 	@$(OBJDUMP) -d -s -h $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
-	@$(OBJCOPY) -S -j .flash -j .ssbl -j .text -j .rodata -j .data -j .bss -O binary $(IMAGE).elf $(IMAGE).bin
+	@$(OBJCOPY) -S -O binary $(IMAGE).elf $(IMAGE).bin
 	@echo + OBJCOPY "->" $(IMAGE)-data.bin
 	@$(OBJCOPY) -S -j .data -O binary $(IMAGE).elf $(IMAGE)-data.bin
 
