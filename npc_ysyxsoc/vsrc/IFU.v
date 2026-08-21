822f33e093786880cc8d765963adc04ac388b9f0
tracer-ysyx <tracer@ysyx.org>
2026-08-21 13:26:20 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-29-generic #29~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 12 17:25:56 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:26:20 up 14:27,  1 user,  load average: 1.89, 1.90, 1.57

--- npc_ysyxsoc/vsrc/IFU.v
+++ npc_ysyxsoc/vsrc/IFU.v
@@ -87,6 +87,7 @@ module ysyx_26010011_IFU(
 			PC <= 32'h80000000;
 			`else
 				`ifdef USE_VERILATOR
+				$display("Warning: Verilator simulation, setting PC to 0x30000000");
 				PC <= 32'h30000000;
 				`endif
 			`endif
