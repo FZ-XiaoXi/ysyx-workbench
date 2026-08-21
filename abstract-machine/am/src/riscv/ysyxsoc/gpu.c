3142026d98a16e66988e23f3148ca339a8022e34
tracer-ysyx <tracer@ysyx.org>
2026-08-21 21:16:31 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-29-generic #29~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 12 17:25:56 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:16:31 up 22:18,  1 user,  load average: 2.97, 3.31, 3.18

--- abstract-machine/am/src/riscv/ysyxsoc/gpu.c
+++ abstract-machine/am/src/riscv/ysyxsoc/gpu.c
@@ -4,10 +4,10 @@
 // #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 static int gpu_w,gpu_h;
-
 void __am_gpu_init() {
   gpu_w = 640;
   gpu_h = 480;
+  memset((void*)FB_ADDR, 0, gpu_w * gpu_h * 4);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
