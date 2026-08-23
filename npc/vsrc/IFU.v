592092ca2c0fb389dcac443b65826117260d95e6
tracer-ysyx <tracer@ysyx.org>
2026-08-23 15:21:00 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-30-generic #30~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:27:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:21:00 up  4:29,  1 user,  load average: 1.55, 1.17, 1.14

--- npc/vsrc/IFU.v
+++ npc/vsrc/IFU.v
@@ -135,7 +135,7 @@ module ysyx_26010011_IFU(
 		end
 	end;
 
-	ysyx_26010011_IFU_icache #(.CACHE_BLOCK_SIZE(4), .CACHE_SIZE(16)) icache_u0(
+	ysyx_26010011_IFU_icache #(.CACHE_SIZE(16)) icache_u0(
 		.clock(clock),
 		.reset(reset),
 
@@ -175,8 +175,8 @@ module ysyx_26010011_IFU(
 
 endmodule
 
+
 module ysyx_26010011_IFU_icache #(
-	parameter CACHE_BLOCK_SIZE = 4,
 	parameter CACHE_SIZE = 16
 )(
 	input	clock,
@@ -199,21 +199,11 @@ module ysyx_26010011_IFU_icache #(
 
 	output           debug_is_hit
 );
-
-// `ifdef FORMAL
-// 	always @(*) begin
-
-// 		c_assert: assert(1 == 1);
-// 	end
-// `endif  // FORMAL
-
+	localparam CACHE_BLOCK_SIZE = 4;
 	localparam BLOCK_W = CACHE_BLOCK_SIZE * 8;
 	localparam INDEX_W = $clog2(CACHE_SIZE);
 	localparam OFFSET_W = $clog2(CACHE_BLOCK_SIZE);
 	localparam TAG_W   = 32 - OFFSET_W - INDEX_W;
-	localparam BURST_LEN = CACHE_BLOCK_SIZE>>2;
-	localparam BURST_W = (|($clog2(BURST_LEN)))?($clog2(BURST_LEN)):1;
-	localparam [BURST_W-1:0] BURST_LAST = BURST_W'(BURST_LEN - 1);
 
 	localparam S_IDLE       = 3'd0;
 	localparam S_WAIT_READY = 3'd1; // 等待地址通道接受地址
@@ -223,7 +213,6 @@ module ysyx_26010011_IFU_icache #(
 	
 	wire [INDEX_W-1:0] now_index;
 	wire [TAG_W-1:0]   now_tag;
-	wire [OFFSET_W:0]now_offset;
 	wire is_hit;
 	reg [BLOCK_W-1:0] cache_mem   [0:CACHE_SIZE-1];
 	reg               cache_valid [0:CACHE_SIZE-1];
@@ -234,11 +223,8 @@ module ysyx_26010011_IFU_icache #(
 	wire ar_fire;
 	wire r_fire;
 
-	reg [BURST_W-1:0] burst_cnt;
-
 	assign now_index = {in_addr[INDEX_W-1+OFFSET_W:0 + OFFSET_W]} ;
 	assign now_tag   = {in_addr[INDEX_W + TAG_W - 1 + OFFSET_W: INDEX_W + OFFSET_W]};
-	assign now_offset = {1'b0, in_addr[OFFSET_W-1:0]};
 	assign is_hit = in_reqValid & cache_valid[now_index] && (cache_tag[now_index] == now_tag);
 	
 	assign debug_is_hit = is_hit;
@@ -255,7 +241,7 @@ module ysyx_26010011_IFU_icache #(
 			if((r_fire & (state == S_WAIT_DATA) & ~pc_flush)) begin
 				cache_valid[now_index] <= r_fire & out_rlast;
 				cache_tag[now_index]   <= now_tag;
-				cache_mem[now_index][burst_cnt*32 +: 32]   <= out_rdata;
+				cache_mem[now_index][31:0]   <= out_rdata;
 			end else if((r_fire & (state == S_WAIT_DATA) & pc_flush)) begin
 				cache_valid[now_index] <= 0;
 			end
@@ -268,7 +254,7 @@ module ysyx_26010011_IFU_icache #(
 
 	assign out_araddr  = in_addr & {{(32-OFFSET_W){1'b1}}, {OFFSET_W{1'b0}}};
 	assign out_arid    = 4'b0;
-	assign out_arlen   = BURST_LEN - 1;
+	assign out_arlen   = 8'b0;
 	assign out_arsize  = 3'b010;
 	assign out_arburst = 2'b01;
 	assign out_arvalid = ((!is_hit & ((state == S_IDLE) && in_reqValid)) || ((state == S_WAIT_READY) || (state == S_WAIT_READY_FLUSH)))&!reset;
@@ -284,31 +270,11 @@ module ysyx_26010011_IFU_icache #(
 		end
 	end
 
-	always @(posedge clock) begin
-		if (reset) begin
-			burst_cnt <= 0;
-		end else if((pc_flush & ~pc_flushed)) begin
-			burst_cnt <= 0;
-		end else if (r_fire) begin
-			if (burst_cnt == BURST_LAST && out_rlast && r_fire) begin
-				burst_cnt <= 0;
-			end else begin
-				burst_cnt <= burst_cnt + {{(BURST_W-1){1'b0}}, 1'b1};
-			end
-		end else if(ar_fire) begin
-			burst_cnt <= 0;
-		end else begin
-			burst_cnt <= burst_cnt;
-		end
-	end
-
 	always @(*) begin
 		if(is_hit) begin
-			in_rdata = cache_mem[now_index][now_offset*8 +: 32];
-		end else if(now_offset[2+BURST_W-1:2] == BURST_LAST)begin
-			in_rdata = out_rdata;
+			in_rdata = cache_mem[now_index][31:0];
 		end else begin
-			in_rdata = cache_mem[now_index][now_offset*8 +: 32];
+			in_rdata = out_rdata;
 		end
 	end
 
@@ -415,3 +381,247 @@ module ysyx_26010011_IFU_icache #(
 	end
 
 endmodule
+
+
+
+
+// module ysyx_26010011_IFU_icache #(
+// 	parameter CACHE_BLOCK_SIZE = 4,
+// 	parameter CACHE_SIZE = 16
+// )(
+// 	input	clock,
+// 	input	reset,
+
+// 	input	flush,
+// 	input	pc_flush/*verilator public*/,
+
+// 	input	[31:0] 		in_addr,
+// 	input				in_reqValid,
+// 	output reg			in_respValid,
+// 	output reg [31:0] 	in_rdata,
+
+
+// 	output [31:0]     out_araddr, output            out_arvalid,input             out_arready,output [3:0]      out_arid,
+// 	output [7:0]      out_arlen,  output [2:0]      out_arsize, output [1:0]      out_arburst,
+
+// 	input  [31:0]     out_rdata,  input  [1:0]      out_rresp,  input             out_rvalid, output            out_rready,
+// 	input             out_rlast,  input  [3:0]      out_rid,
+
+// 	output           debug_is_hit
+// );
+
+// // `ifdef FORMAL
+// // 	always @(*) begin
+
+// // 		c_assert: assert(1 == 1);
+// // 	end
+// // `endif  // FORMAL
+
+// 	localparam BLOCK_W = CACHE_BLOCK_SIZE * 8;
+// 	localparam INDEX_W = $clog2(CACHE_SIZE);
+// 	localparam OFFSET_W = $clog2(CACHE_BLOCK_SIZE);
+// 	localparam TAG_W   = 32 - OFFSET_W - INDEX_W;
+// 	localparam BURST_LEN = CACHE_BLOCK_SIZE>>2;
+// 	localparam BURST_W = (|($clog2(BURST_LEN)))?($clog2(BURST_LEN)):1;
+// 	localparam [BURST_W-1:0] BURST_LAST = BURST_W'(BURST_LEN - 1);
+
+// 	localparam S_IDLE       = 3'd0;
+// 	localparam S_WAIT_READY = 3'd1; // 等待地址通道接受地址
+// 	localparam S_WAIT_DATA  = 3'd2; // 等待数据返回
+// 	localparam S_WAIT_READY_FLUSH = 3'd3;
+// 	localparam S_WAIT_DATA_FLUSH  = 3'd4;
+	
+// 	wire [INDEX_W-1:0] now_index;
+// 	wire [TAG_W-1:0]   now_tag;
+// 	wire [OFFSET_W:0]now_offset;
+// 	wire is_hit;
+// 	reg [BLOCK_W-1:0] cache_mem   [0:CACHE_SIZE-1];
+// 	reg               cache_valid [0:CACHE_SIZE-1];
+// 	reg [TAG_W-1:0]   cache_tag   [0:CACHE_SIZE-1];
+// 	reg pc_flushed/*verilator public*/;
+
+// 	reg [2:0] state, next_state;
+// 	wire ar_fire;
+// 	wire r_fire;
+
+// 	reg [BURST_W-1:0] burst_cnt;
+
+// 	assign now_index = {in_addr[INDEX_W-1+OFFSET_W:0 + OFFSET_W]} ;
+// 	assign now_tag   = {in_addr[INDEX_W + TAG_W - 1 + OFFSET_W: INDEX_W + OFFSET_W]};
+// 	assign now_offset = {1'b0, in_addr[OFFSET_W-1:0]};
+// 	assign is_hit = in_reqValid & cache_valid[now_index] && (cache_tag[now_index] == now_tag);
+	
+// 	assign debug_is_hit = is_hit;
+
+// 	always @(posedge clock) begin
+// 		if (reset | flush) begin
+// 			integer i;
+// 			for (i = 0; i < CACHE_SIZE; i = i + 1) begin
+// 				cache_valid[i] <= 1'b0;
+// 				cache_tag[i]   <= 0;
+// 				cache_mem[i]   <= 0;
+// 			end
+// 		end else begin
+// 			if((r_fire & (state == S_WAIT_DATA) & ~pc_flush)) begin
+// 				cache_valid[now_index] <= r_fire & out_rlast;
+// 				cache_tag[now_index]   <= now_tag;
+// 				cache_mem[now_index][burst_cnt*32 +: 32]   <= out_rdata;
+// 			end else if((r_fire & (state == S_WAIT_DATA) & pc_flush)) begin
+// 				cache_valid[now_index] <= 0;
+// 			end
+// 		end
+// 	end
+
+// 	// 握手成功标志
+// 	assign ar_fire = out_arvalid && out_arready;
+// 	assign r_fire  = out_rvalid && out_rready;
+
+// 	assign out_araddr  = in_addr & {{(32-OFFSET_W){1'b1}}, {OFFSET_W{1'b0}}};
+// 	assign out_arid    = 4'b0;
+// 	assign out_arlen   = BURST_LEN - 1;
+// 	assign out_arsize  = 3'b010;
+// 	assign out_arburst = 2'b01;
+// 	assign out_arvalid = ((!is_hit & ((state == S_IDLE) && in_reqValid)) || ((state == S_WAIT_READY) || (state == S_WAIT_READY_FLUSH)))&!reset;
+// 	assign out_rready  = !reset;
+
+// 	always @(posedge clock) begin
+// 		if(reset) begin
+// 			pc_flushed <= 0;
+// 		end else if(pc_flush) begin
+// 			pc_flushed <= 1;
+// 		end else if(in_respValid) begin
+// 			pc_flushed <= 0;
+// 		end
+// 	end
+
+// 	always @(posedge clock) begin
+// 		if (reset) begin
+// 			burst_cnt <= 0;
+// 		end else if((pc_flush & ~pc_flushed)) begin
+// 			burst_cnt <= 0;
+// 		end else if (r_fire) begin
+// 			if (burst_cnt == BURST_LAST && out_rlast && r_fire) begin
+// 				burst_cnt <= 0;
+// 			end else begin
+// 				burst_cnt <= burst_cnt + {{(BURST_W-1){1'b0}}, 1'b1};
+// 			end
+// 		end else if(ar_fire) begin
+// 			burst_cnt <= 0;
+// 		end else begin
+// 			burst_cnt <= burst_cnt;
+// 		end
+// 	end
+
+// 	always @(*) begin
+// 		if(is_hit) begin
+// 			in_rdata = cache_mem[now_index][now_offset*8 +: 32];
+// 		end else if(now_offset[2+BURST_W-1:2] == BURST_LAST)begin
+// 			in_rdata = out_rdata;
+// 		end else begin
+// 			in_rdata = cache_mem[now_index][now_offset*8 +: 32];
+// 		end
+// 	end
+
+// 	always @(*) begin
+// 		if(in_reqValid) begin
+// 			case (state) 
+// 				S_IDLE: begin
+// 					if(is_hit) begin
+// 						in_respValid = 1;
+// 					end else begin
+// 						in_respValid = 0;
+// 					end
+// 				end
+// 				S_WAIT_READY: begin
+// 					if(is_hit & pc_flushed) in_respValid = 1;
+// 					else in_respValid = 0;
+// 				end
+// 				S_WAIT_DATA: begin
+// 					if(r_fire & out_rlast & ~(pc_flush & ~pc_flushed)) begin
+// 						in_respValid = 1;
+// 					end else begin
+// 						in_respValid = 0;
+// 					end
+// 				end
+// 				S_WAIT_READY_FLUSH: begin
+// 					in_respValid = 0;
+// 				end
+// 				S_WAIT_DATA_FLUSH: begin
+// 					in_respValid = 0;
+// 				end
+// 				default: in_respValid = 0;
+// 			endcase
+// 		end else begin
+// 			in_respValid = 0;
+// 		end
+// 	end
+
+// 	always @(*) begin
+// 		case(state)
+// 			S_IDLE: begin
+// 				if(in_reqValid & is_hit) begin
+// 					next_state = S_IDLE;
+// 				end else if(in_reqValid & ~is_hit) begin
+// 					if (ar_fire) begin
+// 						if((pc_flush & ~pc_flushed))begin
+// 							next_state = S_WAIT_DATA_FLUSH;
+// 						end else begin
+// 							next_state = S_WAIT_DATA;
+// 						end
+// 					end else begin
+// 						next_state = S_WAIT_READY;
+// 					end
+// 				end else begin
+// 					next_state = S_IDLE;
+// 				end
+// 			end
+// 			S_WAIT_READY: begin
+// 				if (ar_fire) begin
+// 					if((pc_flush & ~pc_flushed)) begin
+// 						next_state = S_WAIT_DATA_FLUSH;
+// 					end else begin
+// 						next_state = S_WAIT_DATA;
+// 					end
+// 				end else begin
+// 					if((pc_flush & ~pc_flushed)) begin
+// 						next_state = S_WAIT_READY_FLUSH;
+// 					end else begin
+// 						next_state = S_WAIT_READY;
+// 					end
+// 				end
+// 			end
+// 			S_WAIT_DATA: begin
+// 				if(r_fire & out_rlast)begin
+// 					next_state = S_IDLE;
+// 				end else begin
+// 					if((pc_flush & ~pc_flushed)) begin
+// 						next_state = S_WAIT_DATA_FLUSH;
+// 					end else begin
+// 						next_state = S_WAIT_DATA;
+// 					end
+// 				end
+// 			end
+// 			S_WAIT_READY_FLUSH: begin
+// 				if (ar_fire) begin
+// 					next_state = S_WAIT_DATA_FLUSH;
+// 				end else begin
+// 					next_state = S_WAIT_READY_FLUSH;
+// 				end
+// 			end
+// 			S_WAIT_DATA_FLUSH: begin
+// 				if(r_fire & out_rlast)begin
+// 					next_state = S_IDLE;
+// 				end else begin
+// 					next_state = S_WAIT_DATA_FLUSH;
+// 				end
+// 			end
+// 			default: next_state = S_IDLE;
+// 		endcase
+// 	end
+
+// 	always @(posedge clock) begin
+// 		if (reset) state <= S_IDLE;
+// 		else       state <= next_state;
+// 	end
+
+// endmodule
