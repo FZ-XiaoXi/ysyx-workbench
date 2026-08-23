ab7b535f3d354c67fe36824c3a45a4c4c67f727a
tracer-ysyx <tracer@ysyx.org>
2026-08-23 22:42:39 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-30-generic #30~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:27:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:42:39 up 11:51,  1 user,  load average: 0.67, 0.99, 1.22

--- npc/vsrc/IDU.v
+++ npc/vsrc/IDU.v
@@ -355,7 +355,7 @@ module ysyx_26010011_IDU(
 	
 
 	always @(*) begin
-		if(idu_out_bus_rs1 == exu_out_bus_rd && exu_out_bus_rd_valid && exu_out_bus_bypass_valid) begin
+		if(idu_out_bus_rs1 == exu_out_bus_rd && exu_out_bus_rd_valid && exu_out_bus_bypass_valid && idu_out_bus_rs1 != 5'd0) begin
 			rs1_val_bypass = exu_out_bus_gpr_wdata;
 		// end else if(idu_out_bus_rs1 == lsu_out_bus_rd && lsu_out_bus_rd_valid && lsu_out_bus_bypass_valid) begin
 		// 	rs1_val_bypass = lsu_out_bus_gpr_wdata;
@@ -366,7 +366,7 @@ module ysyx_26010011_IDU(
 		end
 	end
 	always @(*) begin
-		if(idu_out_bus_rs2 == exu_out_bus_rd && exu_out_bus_rd_valid && exu_out_bus_bypass_valid) begin
+		if(idu_out_bus_rs2 == exu_out_bus_rd && exu_out_bus_rd_valid && exu_out_bus_bypass_valid && idu_out_bus_rs2 != 5'd0) begin
 			idu_out_bus_rs2_val = exu_out_bus_gpr_wdata;
 		// end else if(idu_out_bus_rs2 == lsu_out_bus_rd && lsu_out_bus_rd_valid && lsu_out_bus_bypass_valid) begin
 		// 	idu_out_bus_rs2_val = lsu_out_bus_gpr_wdata;
