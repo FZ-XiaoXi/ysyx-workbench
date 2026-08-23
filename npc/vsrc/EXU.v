87c26a90e47f865d3ba873034477ad82bb166a8f
tracer-ysyx <tracer@ysyx.org>
2026-08-23 21:02:03 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-30-generic #30~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:27:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:02:03 up 10:10,  1 user,  load average: 1.62, 0.97, 1.08

--- npc/vsrc/EXU.v
+++ npc/vsrc/EXU.v
@@ -39,8 +39,8 @@ module ysyx_26010011_EXU(
 	output     [31:0]exu_out_bus_dnpc,
 	output reg [31:0]exu_out_bus_csr_result,
 
-	reg [31:0] exu_out_bus_alu_result,
-	reg        exu_out_bus_comp_result,
+	output reg [31:0] exu_out_bus_alu_result,
+	output reg        exu_out_bus_comp_result,
 
 	output reg       exu_out_bus_dnpc_valid,
 	output           exu_out_bus_rd_valid,
