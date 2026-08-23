384853e94af3eaf8c50e960fda2de9cc40284076
tracer-ysyx <tracer@ysyx.org>
2026-08-23 12:32:09 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-30-generic #30~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:27:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:32:09 up  1:40,  1 user,  load average: 1.18, 0.99, 0.97

--- npc/vsrc/CPU.v
+++ npc/vsrc/CPU.v
@@ -116,7 +116,7 @@ module ysyx_26010011(
 `ifdef USE_VERILATOR
 	wire wbu_in_valid,wbu_in_ready,wbu_in_bus_isWGPR;
 	wire [31:0]wbu_in_bus_pc,/*wbu_in_bus_snpc,*/wbu_in_bus_instruction;
-	wire [31:0]wbu_in_bus_lsu_result,wbu_in_bus_alu_result,;
+	wire [31:0]wbu_in_bus_lsu_result,wbu_in_bus_alu_result;
 	wire wbu_in_bus_comp_result,wbu_in_bus_isLOAD,wbu_in_bus_isSTORE,wbu_in_bus_isJUMP,wbu_in_bus_isWCOMP,wbu_in_bus_isBRANCH;
 `endif
 	wire [31:0]wbu_in_bus_csr_result;
