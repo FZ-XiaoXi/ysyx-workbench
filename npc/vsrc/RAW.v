c3b33fdd538e17d03cd97a61be14891df5dcbf07
tracer-ysyx <tracer@ysyx.org>
2026-08-23 21:38:20 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-30-generic #30~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:27:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:38:20 up 10:47,  1 user,  load average: 1.47, 0.89, 0.77

--- npc/vsrc/RAW.v
+++ npc/vsrc/RAW.v
@@ -41,38 +41,24 @@ module ysyx_26010011_RAW(
 );
 	always @(*) begin
         is_RAW = 1'b0;
-
-        if(exu_rd_valid && exu_in_bus_rd != 5'b0) begin
-            if(((rs1 == exu_in_bus_rd) || ((rs2 == exu_in_bus_rd) && rs2_valid))) begin
-                is_RAW = exu_bypass_valid?1'b0:1'b1;
-            end
+        if(exu_rd_valid && exu_in_bus_rd != 5'b0 && ((rs1 == exu_in_bus_rd) || ((rs2 == exu_in_bus_rd) && rs2_valid))) begin
+            is_RAW = exu_bypass_valid?1'b0:1'b1;
         end
-        else if(exu_csr_valid) begin
-            if(csr == exu_in_bus_csr_rd) begin
-                is_RAW = 0?1'b0:1'b1;
-            end
+        else if(exu_csr_valid && (csr == exu_in_bus_csr_rd)) begin
+            is_RAW = 0?1'b0:1'b1;
         end
-        else if(lsu_rd_valid && lsu_in_bus_rd != 5'b0) begin
-            if(((rs1 == lsu_in_bus_rd) || ((rs2 == lsu_in_bus_rd) && rs2_valid))) begin
-                is_RAW = lsu_bypass_valid?1'b0:1'b1;
-            end
+        else if(lsu_rd_valid && lsu_in_bus_rd != 5'b0 && ((rs1 == lsu_in_bus_rd) || ((rs2 == lsu_in_bus_rd) && rs2_valid))) begin
+            is_RAW = lsu_bypass_valid?1'b0:1'b1;
         end
-        else if(lsu_csr_valid) begin
-            if(csr == lsu_in_bus_csr_rd) begin
-                is_RAW = 0?1'b0:1'b1;
-            end
+        else if(lsu_csr_valid && (csr == lsu_in_bus_csr_rd)) begin
+            is_RAW = 0?1'b0:1'b1;
         end
-        else if(wbu_rd_valid && wbu_in_bus_rd != 5'b0) begin
-            if(((rs1 == wbu_in_bus_rd) || ((rs2 == wbu_in_bus_rd) && rs2_valid))) begin
-                is_RAW = 0?1'b0:1'b1;
-            end
+        else if(wbu_rd_valid && wbu_in_bus_rd != 5'b0 && ((rs1 == wbu_in_bus_rd) || ((rs2 == wbu_in_bus_rd) && rs2_valid))) begin
+            is_RAW = 0?1'b0:1'b1;
         end
-        else if(wbu_csr_valid) begin
-            if(csr == wbu_in_bus_csr_rd) begin
-                is_RAW = 0?1'b0:1'b1;
-            end
+        else if(wbu_csr_valid && (csr == wbu_in_bus_csr_rd)) begin
+            is_RAW = 0?1'b0:1'b1;
         end
-        
     end
 endmodule
 
