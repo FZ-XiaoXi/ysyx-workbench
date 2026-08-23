7bb7a3fefcb2434b5274d4a0e9a758ae33b55d02
tracer-ysyx <tracer@ysyx.org>
2026-08-24 01:53:19 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-30-generic #30~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:27:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:53:19 up  2:56,  1 user,  load average: 1.63, 1.76, 1.81

--- npc/vsrc/CPU.v
+++ npc/vsrc/CPU.v
@@ -98,10 +98,11 @@ module ysyx_26010011(
 	wire [ 3:0]idu_out_bus_alu_op;
 	wire [ 1:0]idu_out_bus_comp_op,idu_out_bus_perip_mask;
 	wire [ 2:0]idu_out_bus_opCSR;
-	wire [4:0]idu_out_bus_rd,idu_out_bus_exception;
+	wire [ 4:0]idu_out_bus_exception;
+	wire [ 3:0]idu_out_bus_rd;
 
 	wire exu_in_valid,exu_in_ready,exu_in_bus_isLOAD,exu_in_bus_isSTORE,exu_in_bus_isWGPR,exu_in_bus_isJUMP,exu_in_bus_isWCOMP,exu_in_bus_isBRANCH,exu_in_bus_isUnSigned,exu_in_bus_isUsePC,exu_in_bus_alu_isUseImm,exu_in_bus_comp_isUseImm;
-	wire [4:0]exu_in_bus_rd,exu_out_bus_rd;
+	wire [e:0]exu_in_bus_rd,exu_out_bus_rd;
 	wire [4:0]exu_in_bus_exception;
 	wire [31:0]exu_in_bus_rs1_val,exu_in_bus_rs2_val,exu_in_bus_imm,exu_in_bus_pc ,exu_in_bus_snpc;
 	wire [3:0]exu_in_bus_alu_op;
