ff46614d0949a482898eb977b3c489b13f080fb9
tracer-ysyx <tracer@ysyx.org>
2026-08-24 14:27:01 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-30-generic #30~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:27:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:27:01 up 15:30,  1 user,  load average: 2.87, 2.24, 2.10

--- npc/csrc/monitor/trace.c
+++ npc/csrc/monitor/trace.c
@@ -40,7 +40,7 @@ void dtrace(int addr, bool isWrite, int data){
 
 void itrace(){
     char *p = cpu.logbuf;
-    p += snprintf(p, sizeof(cpu.logbuf), "[%08ld]" FMT_WORD ":", counter_inst, cpu.tb_FINAL_pc);
+    p += snprintf(p, sizeof(cpu.logbuf), "[%08ld]" FMT_WORD ":", cpu.counter_inst, cpu.tb_FINAL_pc);
     uint32_t inst_t = pmem_read(cpu.pc);
     uint8_t *inst = (uint8_t *)&inst_t;
     for (int i = CONFIG_INST_LEN - 1; i >= 0; i --) {
