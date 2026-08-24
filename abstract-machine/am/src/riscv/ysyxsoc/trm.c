0967baf3d93325f2bb847f4a794c33ed5aa6f7c8
tracer-ysyx <tracer@ysyx.org>
2026-08-24 15:07:12 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-30-generic #30~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:27:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:07:12 up 16:10,  1 user,  load average: 2.35, 2.66, 2.43

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -48,15 +48,15 @@ void _trm_init() {
   uint32_t ysyx_name,ysyx_id;
   asm volatile("csrr %0,mvendorid" : "=r"(ysyx_name));
   asm volatile("csrr %0,marchid" : "=r"(ysyx_id));
-  // ioe_init();
+  ioe_init();
   
-  // printf("data:[0x%08x-0x%08x) -> [0x%08x-0x%08x)\n",(uint32_t)(void*)(&_data_lma_start),(uint32_t)(void*)(&_data_lma_end),(uint32_t)(void*)(&_data_vma_start),(uint32_t)(void*)(&_data_vma_end));
-  // printf("bss:[0x%08x-0x%08x) -> [0x%08x-0x%08x)\n",(uint32_t)(void*)(&_bss_lma_start),(uint32_t)(void*)(&_bss_lma_end),(uint32_t)(void*)(&_bss_vma_start),(uint32_t)(void*)(&_bss_vma_end));
-  // printf("heap:[0x%08x-0x%08x)\n",(uint32_t)(void*)(&_heap_start),(uint32_t)(void*)(&_heap_end));
-  // printf("stack:[0x%08x-0x%08x)\n",(uint32_t)(void*)(&_stack_top),(uint32_t)(void*)(&_stack_pointer));
+  printf("data:[0x%08x-0x%08x) -> [0x%08x-0x%08x)\n",(uint32_t)(void*)(&_data_lma_start),(uint32_t)(void*)(&_data_lma_end),(uint32_t)(void*)(&_data_vma_start),(uint32_t)(void*)(&_data_vma_end));
+  printf("bss:[0x%08x-0x%08x) -> [0x%08x-0x%08x)\n",(uint32_t)(void*)(&_bss_lma_start),(uint32_t)(void*)(&_bss_lma_end),(uint32_t)(void*)(&_bss_vma_start),(uint32_t)(void*)(&_bss_vma_end));
+  printf("heap:[0x%08x-0x%08x)\n",(uint32_t)(void*)(&_heap_start),(uint32_t)(void*)(&_heap_end));
+  printf("stack:[0x%08x-0x%08x)\n",(uint32_t)(void*)(&_stack_top),(uint32_t)(void*)(&_stack_pointer));
   
-  // printf("===YSYX:%c%c%c%c\n",(ysyx_name>>24)&0xff,(ysyx_name>>16)&0xff,(ysyx_name>>8)&0xff,ysyx_name&0xff);
-  // printf("===ID:%d\n",ysyx_id);
+  printf("===YSYX:%c%c%c%c\n",(ysyx_name>>24)&0xff,(ysyx_name>>16)&0xff,(ysyx_name>>8)&0xff,ysyx_name&0xff);
+  printf("===ID:%d\n",ysyx_id);
 
   
 
