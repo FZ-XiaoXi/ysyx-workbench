4ef68eb59d9194f4539ea52a6288827bef70100e
tracer-ysyx <tracer@ysyx.org>
2026-08-25 02:08:50 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-30-generic #30~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:27:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:08:50 up 1 day,  3:12,  1 user,  load average: 3.49, 3.10, 2.47

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -80,7 +80,7 @@ extern FILE* log_fp;
     #include "nvboard.h"
 #endif
 
-// #define CONFIG_DIFFTEST_ENABLE
+#define CONFIG_DIFFTEST_ENABLE
 #ifdef CONFIG_DIFFTEST_ENABLE
     // #define CONFIG_DIFFTEST_MEM_ENABLE
 #endif
