b7f19676c69f22f918061a145a56137d640ea8c3
tracer-ysyx <tracer@ysyx.org>
2026-08-24 23:01:27 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-30-generic #30~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:27:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:01:27 up 1 day, 4 min,  1 user,  load average: 2.28, 1.01, 1.12

--- npc/csrc/include/common.h
+++ npc/csrc/include/common.h
@@ -86,7 +86,7 @@ extern FILE* log_fp;
 #endif
 
 // #define CONFIG_WATCHPOINT_ENABLE
-#define CONFIG_WAVE_ENABLE
+// #define CONFIG_WAVE_ENABLE
 
 
 #define CONFIG_TRACE_ENABLE
@@ -94,7 +94,7 @@ extern FILE* log_fp;
 
     #define CONFIG_ITRACE_ENABLE
     #ifdef CONFIG_ITRACE_ENABLE
-        #define CONFIG_ITRACE_PRINT
+        // #define CONFIG_ITRACE_PRINT
     #endif
     
     // #define CONFIG_MTRACE_ENABLE
