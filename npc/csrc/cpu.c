4497f4000cf027dcfd930046b0554eb75b5d9094
tracer-ysyx <tracer@ysyx.org>
2026-08-24 21:01:20 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-30-generic #30~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:27:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:01:20 up 22:04,  1 user,  load average: 3.08, 2.41, 2.00

--- npc/csrc/cpu.c
+++ npc/csrc/cpu.c
@@ -160,6 +160,7 @@ void cpu_exec(uint64_t n){
 	{
 		case NPC_ABORT:
 		case NPC_END: {
+			DUMP();DUMP();DUMP();DUMP();DUMP();
 			bool success = true;
 			cpu.halt_ret = reg_str2val("$a0", &success);
 			Assert(success, "Cannot to get the value of '$a0'");
