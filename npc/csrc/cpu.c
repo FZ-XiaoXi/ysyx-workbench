fe726a9c559c9571ff0106fb905194ded749c2b3
tracer-ysyx <tracer@ysyx.org>
2026-08-24 14:29:43 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-30-generic #30~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:27:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:29:43 up 15:33,  1 user,  load average: 2.71, 2.45, 2.21

--- npc/csrc/cpu.c
+++ npc/csrc/cpu.c
@@ -76,7 +76,6 @@ void cpu_exec(uint64_t n){
 	// int this_cnt = 0;
 	// static uint64_t cyc_cnt = 0;
 	while(n > 0){
-		n--;
 		cpu_exec_once();
 		#ifdef CONFIG_NVBOARD_ENABLE
 			nvboard_update();
@@ -119,7 +118,7 @@ void cpu_exec(uint64_t n){
 		if(cpu.tb_isFINAL){
 			// this_cnt = 0;
 			// Log("PC=" FMT_WORD , cpu.pc);
-			// n--;
+			n--;
 			cpu.counter_inst++;
 			if(cpu.tb_FINAL_inst==_EBREAK && cpu.tb_isFINAL){
 				ebreak();
