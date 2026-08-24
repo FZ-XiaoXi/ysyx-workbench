4717e17851274acf5e43535d2802ff0d7b710ee1
tracer-ysyx <tracer@ysyx.org>
2026-08-24 21:34:42 +0800

    > sim RTL ysyx_26010011 邓烨曦 Linux seaber-ROG-Strix-G16-G614PR-G614PR 7.0.0-30-generic #30~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug  7 13:27:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:34:42 up 22:38,  1 user,  load average: 1.54, 1.71, 1.56

--- npc/csrc/cpu.c
+++ npc/csrc/cpu.c
@@ -117,7 +117,7 @@ void cpu_exec(uint64_t n){
 		}
 		if(cpu.tb_isFINAL){
 			// this_cnt = 0;
-			Log("PC=" FMT_WORD "INST=" FMT_WORD, cpu.pc, cpu.tb_FINAL_inst);
+			Log("PC=" FMT_WORD "INST=" FMT_WORD, cpu.tb_FINAL_pc, cpu.tb_FINAL_inst);
 			n--;
 			// cpu.FINAL_inst = cpu.tb_FINAL_inst;
 			cpu.counter_inst++;
